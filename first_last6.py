31174b5c840714d517fe07812f47c6d478c7f6be
dhelms33 <derrickhelms10@gmail.com>
2024-08-25 22:40:58 -0600

    Added first_last6

---
+++ first_last6.py
@@ -0,0 +1,4 @@
+def first_last6(nums): #function declaration
+  if nums[0] == 6 or nums[-1] == 6: #if the first or last element is 6
+      return True #return true
+  return False #otherwise return false
