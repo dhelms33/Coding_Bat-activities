b11295b866869bdd1ba4bef6e2d47ed3bbb623a1
dhelms33 <dereckhelmsdev@gmail.com>
2025-06-30 21:59:37 -0600

    Added comment

--- lucky_sum.py
+++ lucky_sum.py
@@ -1,5 +1,5 @@
 def lucky_sum(a,b,c):
-  if (a == 13):
+  if (a == 13): #independent ifs check each one can also be done with one if and two specific elif statements
     return 0
   if (b == 13):
     return a
