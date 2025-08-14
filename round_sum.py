4537179420462e6448ca231078e21ba4dd23c380
dhelms33 <derrickhelms10@gmail.com>
2025-08-14 13:25:35 -0600

    Added new functions for round sum and round 10

--- round_sum.py
+++ round_sum.py
@@ -1,7 +1,15 @@
-def round_10(num):
-  zero_value = 0
-  if abs(num[-1]) < 5:
-    zero_value = 0
-  else:
-    return num
-    return num[0] + zero_value
+def round10(num):
+  """ """
+  diff_until_10 = (num // 10 + 1) *10
+  # get last number out of num using % 10, this will always be last digit
+  if num % 10 > 5:
+    num = num + diff_until_10
+  elif num % 10 < 5:
+    num = num - diff_until_10
+  return num
+  
+def round_sum(a,b,c):
+  round_a = round10(a)
+  round_b = round10(b)
+  round_c = round10(c)
+  return round_a + round_b + round_c
