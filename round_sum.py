371db4203b98101aac15f2396f4aa8556b058e77
dhelms33 <derrickhelms10@gmail.com>
2025-08-15 16:05:27 -0600

    Added corrected logic to the round_10 function

--- round_sum.py
+++ round_sum.py
@@ -1,14 +1,21 @@
 def round10(num):
-  """ """
-  diff_until_10 = (num // 10 + 1) *10
+  
+  """ rounds an integer to the nearest multiple of 10
+  if number is greater than or equal to 5, round up. Otherwise round down."""
+  last_digit = num % 10
+# diff_until_10 = (num // 10 + 1) *10
   # get last number out of num using % 10, this will always be last digit
-  if num % 10 > 5:
-    num = num + diff_until_10
-  elif num % 10 < 5:
-    num = num - diff_until_10
-  return num
+  if last_digit % 10 >= 5:
+    return num + (10-last_digit)
+  else:
+    return num - last_digit
+
+  # elif num % 10 < 5:
+  #   num = num - diff_until_10
+  # return num
   
 def round_sum(a,b,c):
+  """Returns the sum of three integers after the round."""
   round_a = round10(a)
   round_b = round10(b)
   round_c = round10(c)
