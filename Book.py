1f9902ffce87a8f3005e7a01ebcd13339c68dbd6
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2025-09-25 18:46:24 -0600

    Create Book.py

---
+++ Book.py
@@ -0,0 +1,13 @@
+class Book:
+  def __init__(self, author, title, description):
+    self.author = author
+    self.title = title
+    self.description = description
+    
+  def get_title(self):
+    return self.title
+    
+  def set_rating():
+    if len(title) <=0:
+      raise ValueError("Title cannot be null.")
+      
